fc64441d81e40cccf819b7cd3e21f8df459cde7e
tronaluz <lucas_tron@hotmail.com>
2024-01-29 16:27:50 -0300

    Update user authentication logic

---
+++ app.py
@@ -0,0 +1,16 @@
+num1 = 5
+num2 = 3.5
+
+a = float(num1)
+b = int(num2)
+
+a = int(float("5.3"))
+b = "3.5"
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 ** num2)
+print(num1 // num2)
