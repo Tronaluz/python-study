7989fdc41555ecf83a6cca689718e7774e7345f1
tronaluz <lucas_tron@hotmail.com>
2024-01-29 16:45:49 -0300

    Add math functions and examples to app.py

--- app.py
+++ app.py
@@ -7,10 +7,29 @@ b = int(num2)
 a = int(float("5.3"))
 b = "3.5"
 
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
-print(num1 % num2)
-print(num1 ** num2)
-print(num1 // num2)
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+# print(num1 // num2)
+
+# print(3 + 5 * 7 +3)
+# print((3 + 5) * (7 +3))
+
+
+print(abs(-15))
+print(pow(3,3))
+print(max(1,85,17,-55,5))
+print(min(1,85,17,-55,5))
+
+print(round(8.3))
+
+import math
+
+print(math.floor(8.8))
+print(math.ceil(8.8))
+print(math.sqrt(9))
+
+
