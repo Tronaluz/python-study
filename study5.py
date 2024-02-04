3702c3bb802c753d3f6e00841493a0f01e2bd8ed
Tronaluz <lucas_tron@hotmail.com>
2024-02-04 20:39:31 -0300

    Atualizações de código para correção de bugs e melhorias de desempenho.

---
+++ study5.py
@@ -0,0 +1,19 @@
+# nome = input("Digite seu nome aqui: ")
+# idade = int(input(f"Qual a sua idade, {nome} ?: "))
+
+# nascimento = 2024 - idade
+
+
+# print(f"Seu nome é {nome}, e sua idade é: {idade} ")
+
+# print(f"{nome} você nasceu {nascimento}")
+
+
+
+# ----calculadora simples----
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+resultado = num1 + num2
+print(f"O resultado é: {resultado}")
+
