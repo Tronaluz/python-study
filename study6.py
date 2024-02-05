bf54c2509e6d32f9db08da801679083f6b92ad71
Tronaluz <lucas_tron@hotmail.com>
2024-02-04 21:46:48 -0300

    Atualizações de código para correção de bugs e melhorias de desempenho.

---
+++ study6.py
@@ -0,0 +1,49 @@
+familia = ["Lucas", "Rosenio", "Nalva", "Gustavo"]
+
+# print(familia[2]) - retorna um indice especifico
+# print(familia[-3]) - retorn um indice especifico de traz para frente
+# print(familia[1:]) - retorna um indice especifico e todos os outros apos ele
+# print(familia[1:3]) - retorna a partir do indice 1 e ate o indice 3, excluindo o indice 3
+
+
+# familia[2] = "Lucinalva" - altera um indice especifico
+# familia.extend(["José", "Célia"]) - adiciona mais de um elemento na lista
+
+
+# familia.append("Clara") - adiciona apenas um elemento na lista
+# familia.insert(1, "Clara") - adiciona um elemento em um indice(determinado  lugar) especifico
+# familia.pop() - remove o ultimo elemento da lista
+# familia.remove("Rosenio")  remove um elemento especifico
+# familia.clear() - remove todos os elementos da lista
+# print(familia.index("Nalva")) - retorna o indice de um elemento especifico
+# print(familia.count("Nalva")) - retorna a quantidade de vezes que um elemento aparece na lista
+
+# idade_familia = [23, 50, 46, 14] - lista de idades
+# idade_familia.sort() - ordena a lista
+# idade_familia.reverse() - inverte a lista
+# familia.sort() - ordena a lista
+# familia.reverse() -  inverte a lista
+
+
+# ---Copiar uma lista por referencia ---
+# familia2 = familia - copia a lista por referencia
+# print(familia2)
+# familia.remove("Lucas") - remove um elemento da lista
+# print(familia2)
+
+# ---Copia de uma lista ---
+# familia2 = familia.copy() -   copia a lista
+# familia.remove("Lucas") - remove um elemento da lista
+# print(familia)
+# print(familia2)
+
+# --Tuple: Imutavel--
+# para crriar um tuple, basta usar parenteses ao inves de colchetes []
+# coordenadas = [-49,-36] - sem tuple
+# coordenadas = (-49,-36) - com tuple
+# coordenadas.pop() - não é possivel remover um elemento de um tuple
+
+
+
+
+
