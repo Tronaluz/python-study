9e0872ac18aa1ddc231138eab3d41f0e68eaff22
Tronaluz <lucas_tron@hotmail.com>
2024-02-11 13:58:45 -0300

    Adiciona função para encontrar o maior número em uma lista

---
+++ study7.py
@@ -0,0 +1,61 @@
+
+# --Primeira função sem nem um atributo para dentro dela
+# def big_mac ():
+#     print("Sanduiche big mac")
+
+# big_mac()
+
+
+
+# --Segunda fração com um atributo para dentro dela, exemplo: {nome}
+# def fazer_big_mac(nome):
+#     print(f"Sanduiche big mac {nome}")
+
+
+# fazer_big_mac("João")
+# fazer_big_mac("Maria")
+# fazer_big_mac("José")
+
+# --Terceira fração com um atributo para dentro dela, exemplo: {nome}
+# def fazer_big_mac(nome):
+#     print(f"Sanduiche big mac {nome}")
+
+# --Quarta fração com um atributo para dentro dela, exemplo: {tamanho}
+# def fazer_batata(tamanho):
+#     print(f"batata {tamanho}")
+
+# --Quinta fração com mais de um atributo para dentro dela, exemplo: {tipo} {tamanho}
+# def preparar_refrigerante(tipo,tamanho):
+#     print(f"{tipo} {tamanho}")
+
+# fazer_big_mac("João")
+# fazer_batata("Grande")
+# preparar_refrigerante("Coca-Cola", "Média")
+
+
+#  Sexta função que recebe varios atributos, e dentro dela, chama-se outras funções
+# def fazer_combo_big_mac(nome, tamanho_batata, tipo_refri, tamanho_refri):
+#     fazer_big_mac(nome)
+#     fazer_batata(tamanho_batata)
+#     preparar_refrigerante(tipo_refri, tamanho_refri)
+
+# fazer_combo_big_mac("Joáo", "Grande", "Coca-Cola", "Média")
+
+
+# def soma_num(num1, num2):
+#     return num1 + num2
+
+# resultado = soma_num(15, 20)
+# print(resultado) 
+
+
+def maior_numero(lista_num):
+    lista_num.sort()
+    lista_num.reverse()
+    maior_num = lista_num[0]
+    return maior_num
+
+
+resultado -  maior_num({10},  {20}, {30}, {40}, {50})
+
+print(resultado)
